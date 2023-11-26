fb33213932bb44bcb76a2a02e18c3837cb805826
Tommaso Tamagni <tommitam03@gmail.com>
2023-11-26 17:46:54 +0100

    Creazione classe MiniatureDetection Creazione classe e init.

---
+++ miniatureDetection.py
@@ -0,0 +1,15 @@
+import torch
+import numpy as np
+import cv2
+from time import time
+
+
+class MiniatureDetection:
+
+    def __init__(self, capture_index, model_name):
+        
+        self.capture_index = capture_index
+        self.model = self.load_model(model_name)
+        self.classes = self.model.names
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        print("Device: ", self.device)
